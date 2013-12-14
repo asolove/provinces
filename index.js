8acc7de74b5da491f2f03356535de9474ee569c1
Adam Solove <asolove@gmail.com>
2013-12-14 10:54:59 -0500

    Scroll design. Animating through years.

--- index.js
+++ index.js
@@ -18,51 +18,87 @@ var dynasties = Object.keys(dynastyDates).map(function(name) {
     return { name: name, start: dynastyDates[name][0], end: dynastyDates[name][1] };
 });
 
-var selected = "Han"; // TODO calc from mouse
-var year = 300;
+var year = -221;
 var layer;
+var scale;
+var chart;
+var animating = true;
 
 var drawDynastyAxes = function() {
-    var chart = d3.select("#dynasties");
+    chart = d3.select("#dynasties");
 
-    var scale = d3.scale.linear()
+    scale = d3.scale.linear()
         .domain([dynasties[0].start, dynasties[dynasties.length-1].end])
-        .range([0, chart.attr("width")]);
+        .range([0, chart.attr("height")]);
         
     var bar = chart.selectAll("g")
         .data(dynasties)
         .enter().append("g")
-            .attr("transform", function(d){ return "translate("+scale(d.start)+",0)"; });
+            .attr("transform", function(d){ return "translate(0,"+scale(d.start)+")"; });
 
     bar.append("rect")
-        .attr("height", 6)
-        .attr("width", function(d){ return scale(d.end) - scale(d.start); });
-
-    bar.append("text")
-        .attr('transform', 'translate(0,20)')
-        .text(function(d){ return d.name; });
-
-    bar.classed("selected", function(d){ return d.name == selected; });
+        .attr("width", 6)
+        .attr("height", function(d){ return scale(d.end) - scale(d.start); });
 
     var dragging = false;
-    chart.on('mousedown', function(){ dragging = true; });
+    chart.on('mousedown', function(){ animating = false; dragging = true; });
     d3.select("body").on("mouseup", function(){ dragging = false; });
     chart.on('mousemove', function(d){
         if (!dragging) return;
-        year = scale.invert(d3.event.x);
-        layer.setFilter(filterByYear(year));
+        year = scale.invert(d3.event.offsetY);
+        update(year);
     });
 
 };
 
+var update = function(y) {
+    year = y;
+
+    layer.setFilter(filterByYear(year));
+
+    var marker = chart.selectAll("g.marker")
+        .data([year])
+        .attr("transform", function(d) { return "translate(0,"+scale(d)+")"; });
+
+    marker.select("text")
+        .text(formatYear);
+
+    marker.enter().append("g")
+            .classed("marker", true)
+            .append("text")
+            .select("text")
+                .attr("transform", "translate(30,0)");
+};
+
+var formatYear = function(year) {
+    return year < 0 ? ""+(-year)+" BCE" : year+" CE";
+};
+
+
 var init = function() {
     drawDynastyAxes();
-    var map = L.mapbox.map('map', 'asolove.gh727mie')
-        .setView([29.7, 113.4], 6);
+    var map = L.mapbox.map('map', 'asolove.pirate-map', { zoomControl: false })
+        .setView([29.7, 113.4], 5);
+    var myStyle = {
+        "color": "#ff0",
+        "weight": 2,
+        "opacity": 0.65,
+        "fillColor": "#0f0"
+    };
 
-    layer = L.mapbox.markerLayer().addTo(map);
-    layer.loadURL("prov_pgn.geojson");
-    layer.setFilter(filterByYear(year));
+    d3.json("prov_pgn.geojson", function(data) {
+        layer = L.mapbox.markerLayer(data).addTo(map);
+        layer.setStyle(myStyle);
+        layer.options.style = myStyle;
+
+        animate(layer);
+    });
+};
+
+var animate = function() {
+    update(year+1);
+    if(!animating || year > 1278) return;
+    setTimeout(animate, 32);
 };
 
 var filterByYear = function(year) {
